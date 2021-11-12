c91aa6c5b12637b98ab6a24d48987b71a49e88a6
wqq <2685919120@qq.com>
2021-11-12 18:02:11 +0800

    second commit

---
+++ src/main/java/com/wqq/GitTest.java
@@ -0,0 +1,14 @@
+package com.wqq;
+
+/**
+ * @ClassName GitTest
+ * @Description GitTest
+ * @Author 26859
+ * @Date 2021/11/12 18:00
+ * @Version 1.0
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
