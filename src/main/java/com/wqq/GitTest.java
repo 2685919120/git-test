094316e0c9e988a7860624d802f83e68126bd0ac
wqq <2685919120@qq.com>
2021-11-12 18:33:19 +0800

    clone commit1

--- src/main/java/com/wqq/GitTest.java
+++ src/main/java/com/wqq/GitTest.java
@@ -16,5 +16,6 @@ public class GitTest {
         System.out.println("hot-fix test11");
         System.out.println("master test 11");
         System.out.println("pull test");
+        System.out.println("clone test");
     }
 }
