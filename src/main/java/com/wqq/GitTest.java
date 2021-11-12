7295090eb819c2cc58d25e1bb881612b447a3aaa
wqq <2685919120@qq.com>
2021-11-12 18:08:04 +0800

    master commit1

--- src/main/java/com/wqq/GitTest.java
+++ src/main/java/com/wqq/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git 11");
         System.out.println("hot-fix test");
+        System.out.println("master test");
     }
 }
